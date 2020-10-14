802c8ac5457380c0885630410247acbf7673876a
chatflip <oo.chat.flip@gmail.com>
2020-10-15 02:22:48 +0900

    rename input image

--- sample.py
+++ sample.py
@@ -3,7 +3,7 @@ import os
 from visualizer import Visualizer
 
 if __name__ == '__main__':
-    src_path = 'sample.jpg'
+    src_path = 'giraffe.jpg'
     dst_path = 'output'
     os.makedirs(dst_path, exist_ok=True)
     vis = Visualizer(src_path, dst_path)
